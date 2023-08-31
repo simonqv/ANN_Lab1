e93677f7891fffa653adc350f1bfc3117af97ee0
Simon Larspers Qvist <simonqv@kth.se>
2023-08-31 10:21:01 +0200

    working on shuffle

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# 3.1.1
 def generate_data():
     n = 100
     # mean point in distribution, X-axis and Y-axis means.
@@ -25,5 +26,25 @@ def generate_data():
     plt.legend()
     plt.show()
 
+
+
+# 3.1.2.1
+def perceptron_learning(classA, classB):
+    # targets[A,B]
+    targets = [np.ones(100), np.zeros(100)]
+    eta = 0.01
+    e = 1
+    xA = [classA[0], classA[1], np.ones(100).tolist()]
+    xB = [classB[0], classB[1], np.ones(100).tolist()]
+    weights = eta * e * x
+
+
+def delta_learning():
+    # targets[A,B]
+    targets = [np.ones(100), (np.ones(100) * -1)]
+    eta = 0.01
+    e = 1
+    x = 1
+    weights = eta * e * x
 
 generate_data()
